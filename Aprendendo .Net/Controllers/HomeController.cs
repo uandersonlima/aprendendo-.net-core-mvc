5a29b1f6e15f699b63aad3441c83d4fc76e8b380
Uanderson Lima Santos <uandersonlimacs@gmail.com>
2019-05-28 14:52:48 -0300

    Primeiro Controlador e Páginas Razor testes

---
+++ Aprendendo .Net/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Aprendendo_.Net.Models;
+
+namespace Aprendendo_.Net.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About ()
+        {
+            ViewData["Message"] = "Aprendendo a programar em Asp.Net.";
+
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "71996677884";
+
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
